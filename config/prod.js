7ad1e8c2872d214ccf4fa58634f54c3482c51e48
Stephen Freeman <stephen.fj7@gmail.com>
2020-04-16 10:18:26 -0600

    Removing Mongo URI

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,3 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI
-    // mongoURI: 'mongodb+srv://FJ_1:UDX61TlaViCG8cDl@watchingvideosocialmedia-f0dn5.mongodb.net/test?retryWrites=true&w=majority'
 };
