e26bbd30c78d6fc8ac9ba9b09c238e067e8bf460
Stephen Freeman <stephen.fj7@gmail.com>
2020-04-18 23:02:47 -0600

    Commenting out files to see if it helps to load the application. Also, last checkin added functionality to load app in heroku

--- routes/api/login.js
+++ routes/api/login.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const router = express.Router();
-
-//@route    GET api/login
-//@desc     Test route
-//@access   Public
-router.get('/', (req, res) => res.send('Login route'));
-
-module.exports = router;
+// const express = require('express');
+// const router = express.Router();
+//
+// //@route    GET api/login
+// //@desc     Test route
+// //@access   Public
+// router.get('/', (req, res) => res.send('Login route'));
+//
+// module.exports = router;
--- routes/api/show.js
+++ routes/api/show.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const router = express.Router();
-
-//@route    GET api/show
-//@desc     Test route
-//@access   Public
-router.get('/', (req, res) => res.send('Show route'));
-
-module.exports = router;
+// const express = require('express');
+// const router = express.Router();
+//
+// //@route    GET api/show
+// //@desc     Test route
+// //@access   Public
+// router.get('/', (req, res) => res.send('Show route'));
+//
+// module.exports = router;
