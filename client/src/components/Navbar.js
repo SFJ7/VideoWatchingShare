c6b76bccd19df3e1a1d074a303709f41b098c98b
Stephen Freeman <stephen.fj7@gmail.com>
2020-05-12 00:07:38 -0600

    Added react strap to use components with bootstrap in react
    Added "Login" and "Logout" to make the authentication clearer
    
    Added responsive navbar for difference screen sizes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {logout} from "../actions/authAction";
+import {Button, Collapse} from 'reactstrap';
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
     const dispatch = useDispatch();
     const {isAuthenticated} = useSelector(state => state.authReducer);
+
+    const toggle = () => setIsOpen(!isOpen);
 
     const logoutHandler = () => {
         dispatch(logout());
@@ -34,48 +38,51 @@ const Navbar = () => {
                                 <div className="search-link"><i className="ri-search-line"/></div>
                             </form>
                         </div>
-                        <button className="navbar-toggler" type="button" data-toggle="collapse"
-                                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        <Button className="navbar-toggler" onClick={toggle}
                                 aria-label="Toggle navigation">
                             <i className="ri-menu-3-line"/>
-                        </button>
-                        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-                            <ul className="navbar-nav ml-auto navbar-list">
-                                <li>
-                                    <Link to={'/profile'} className='iq-waves-effect d-flex align-items-center'>
-                                        <img
-                                            src='https://i.pinimg.com/originals/a3/9d/e1/a39de166cf1b83d8438a6f1b86279604.png'
-                                            className='img-fluid rounded-circle mr-3' alt="user"/>
-                                        <div className='caption'>
-                                            <h6 className="mb-0 line-height">Your Name</h6>
-                                        </div>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="/" className="iq-waves-effect d-flex align-items-center">
-                                        <i className="ri-home-line"/>
-                                    </Link>
-                                </li>
-                                <li className='nav-item'>
-                                    <Link to='/friends' className='search-toggle iq-waves-effect'>
-                                        <i className="ri-group-line"/>
-                                    </Link>
-                                </li>
-                                {isAuthenticated ?
-                                <li className='nav-item'>
-                                    <a onClick={logoutHandler} className='search-toggle iq-waves-effect'>
-                                        <i className="ri-logout-box-line ml-2"/>
-                                    </a>
-                                </li>
-                                    :
-                                    <li className='nav-item'>
-                                        <Link to='/auth' className='search-toggle iq-waves-effect'>
-                                            <i className="ri-login-box-fill ml-2"/>
-                                        </Link>
-                                    </li>
-                                }
-                            </ul>
-                        </div>
+                        </Button>
+                        <Collapse isOpen={isOpen} className='collapse navbar-collapse'>
+                                    <div id='navbarSupportedContent'>
+                                        <ul className="navbar-nav ml-auto navbar-list">
+                                            <li>
+                                                <Link to={'/profile'}
+                                                      className='iq-waves-effect d-flex align-items-center'>
+                                                    <img
+                                                        src='https://i.pinimg.com/originals/a3/9d/e1/a39de166cf1b83d8438a6f1b86279604.png'
+                                                        className='img-fluid rounded-circle mr-3' alt="user"/>
+                                                    <div className='caption'>
+                                                        <h6 className="mb-0 line-height">Your Name</h6>
+                                                    </div>
+                                                </Link>
+                                            </li>
+                                            <li>
+                                                <Link to="/" className="iq-waves-effect d-flex align-items-center">
+                                                    <i className="ri-home-line"/>
+                                                </Link>
+                                            </li>
+                                            <li className='nav-item'>
+                                                <Link to='/friends' className='search-toggle iq-waves-effect'>
+                                                    <i className="ri-group-line"/>
+                                                </Link>
+                                            </li>
+                                            {isAuthenticated ?
+                                                <li className='nav-item'>
+                                                    <Link to={'/'} onClick={logoutHandler}
+                                                       className='search-toggle iq-waves-effect'>
+                                                        Logout<i className="ri-logout-box-line ml-2"/>
+                                                    </Link>
+                                                </li>
+                                                :
+                                                <li className='nav-item'>
+                                                    <Link to='/auth' className='search-toggle iq-waves-effect'>
+                                                        Login<i className="ri-login-box-fill ml-2"/>
+                                                    </Link>
+                                                </li>
+                                            }
+                                        </ul>
+                                    </div>
+                        </Collapse>
                     </nav>
                 </div>
             </div>
